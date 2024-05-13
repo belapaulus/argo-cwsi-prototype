a9ff45142f544de73deef4cbc4d97a92d5b92acf
Béla Anton Paulus <bela@bpalu.com>
2024-05-13 18:25:34 +0200

    add cws to build system

--- cws/start.sh
+++ cws/start.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
+set -eu
 
-kubectl apply -f serviceaccount.yaml
-kubectl apply -f clusterrole.yaml
-kubectl apply -f clusterrolebinding.yaml
-kubectl apply -f cws.yaml
-kubectl apply -f service.yaml
+kubectl delete serviceaccount cwsaccount
+kubectl delete clusterrole cwsrole
+kubectl delete clusterrolebinding cwsbinding
+kubectl delete pod workflow-scheduler
+#kubectl delete service workflow-scheduler
+pids=$(lsof -t -s TCP:LISTEN -i ":8081" || true)
+
+if [ "$pids" != "" ]; then
+  kill $pids
+fi
+
+CWSDIR=cws
+kubectl apply -f $CWSDIR/serviceaccount.yaml
+kubectl apply -f $CWSDIR/clusterrole.yaml
+kubectl apply -f $CWSDIR/clusterrolebinding.yaml
+kubectl apply -f $CWSDIR/cws.yaml
+#kubectl apply -f $CWSDIR/service.yaml
+#
 kubectl wait --for=condition=ready --timeout=-1s pod workflow-scheduler
-kubectl port-forward --address 0.0.0.0 workflow-scheduler 8081:8080
-
+kubectl port-forward workflow-scheduler 8081:8080 &
